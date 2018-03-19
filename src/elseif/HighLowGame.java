b8a9a3d38c170868176f8a5790a763c6d16907c7
League <League@AdminisorsiMac2.attlocal.net>
2018-03-18 17:35:29 -0700

    High Low Game and Robot Color Chooser

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,29 +5,45 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
-		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
-		
-		// 2. Print out the random variable above
-		
 		// 11. Repeat steps 1 to 10 ten times
-		
-			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+		for (int i = 1; i <= 10; i++) {
 			
+		
+		// 3. Change this line to give you a random number between 1 - 100. 
+          int random = new Random().nextInt(100)+1;
+			// 1. Ask the user for a guess using a pop-up window, and save their response 
+		String  pop = JOptionPane.showInputDialog("What number am I thinking of?");
+			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			int too = Integer.parseInt(pop);
 			// 5. if the guess is correct
+		if (too == random) {
+			JOptionPane.showMessageDialog(null, "You WIN!");
+			System.exit(0);
+		}
 				// 6. Win
+		
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+		
 			// 7. if the guess is high
+		if (too>random) {
+			JOptionPane.showMessageDialog(null, "Too high. You lose.");
+		}
 				// 8. Tell them it's too high
+		
 			// 9. if the guess is low
+		if (too<random) {
+			JOptionPane.showMessageDialog(null, "Too low. You lose.");
+		}
 				// 10. Tell them it's too low
-
+	}
 		// 13. Tell them they lose
+		
+		
 		
 	}
 
