264ecdacc2fd58949de8dae02c29738179228330
emerson20 <league@WTS14-4.attlocal.net>
2018-04-14 11:29:35 -0700

    Dizzy Robot, Shiny Objects, Walk Of Fame, FizzBuzz

--- src/extras/DizzyRobot.java
+++ src/extras/DizzyRobot.java
@@ -3,23 +3,29 @@
 
 package extras;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
-	public static void main(String[] args) {
-		
-		 // 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
 
- // 1. Use the dance method to make the robot spin.
-	
+	public static void main(String[] args) {
+
+		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number
+		// of times.
+		String you = JOptionPane.showInputDialog("How dizzy do you want the robot to be from 1-10?");
+		int too = Integer.parseInt(you);
+		// 1. Use the dance method to make the robot spin.
+		robot.setSpeed(60);
+		dance(too);
 
 	}
 
 	static void dance(int numberOfSpins) {
 		for (int i = 0; i < numberOfSpins; i++) {
 			robot.turn(360);
+
 		}
 	}
 }
-
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,17 +9,30 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(199);
+		rob.setY(200);
 		// 2. Make the robot draw a star shape. Hint: 144.
-
+		rob.setSpeed(100);
+		rob.penDown();
+		rob.setPenWidth(10);
+		rob.setRandomPenColor();
+		rob.turn(144);
+		rob.move(300);
+		rob.turn(144);
+		rob.move(300);
+		rob.turn(144);
+		rob.move(300);
+		rob.turn(144);
+		rob.move(300);
+		rob.turn(144);
+		rob.move(300);
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
